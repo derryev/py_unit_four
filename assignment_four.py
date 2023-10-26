5752d875ccf364c1e5f5c394363ee44f82655f2f
derryev <derryev@ssfs.org>
2023-10-26 11:53:45 -0400

    Working on 21 game v3.

--- assignment_four.py
+++ assignment_four.py
@@ -1,6 +1,6 @@
 # Eva D.
-# date
-# description
+# 10/26/2023
+# Executes a game of 21 in which the user and dealer draw randomly generated cards.
 
 import random
 
@@ -27,6 +27,8 @@ def get_winner(user_total, dealer_total):
         return "You win!"
 
 def main():
+
+    print("Welcome to  21! Try to get enough points from the cards you draw to beat the dealer without going over 21")
     # assigns variables to user's first two cards so that program can inform user what cards they drew
     user_card_1 = float(get_card())
     user_card_2 = float(get_card())
@@ -39,22 +41,27 @@ def main():
         new_card = float(get_card())
         user_total = user_total + new_card
         print("You drew a "+str(new_card)+". Your total is now "+str(user_total))
+        # statement to end loop if the user drew too many cards, either reaching 21 and not needing to draw more
+        # or by surpassing 21 which would be an automatic loss (disabling them from drawing more cards)
         if float(user_total) >= 21:
             break
+        # if user has under 21 cards, asks if they want to continue drawing and repeats or ends while loop
         else:
             draw_another = input("Would you like to draw another card (Y or N)? ")
-   # check for after loop ends to see if the user drew too many cards, either reaching 21 and unable to draw more
-    # or by surpassing 21 which would be an automatic loss
+    # if statement to see if user automatically lost before drawing dealer cards and calculating a winner
     if user_total>21:
         print("Looks like you went over 21... You lose!")
-   # if the user didn't reach 21, this has the dealer draw 2 cards, informs the user on what the dealer drew,
-    # and then delivers the verdict on who won by calling the get_winner function
+    # if statement to see if user automatically won before drawing dealer cards and calculating a winner
+    elif user_total==21:
+        print("Looks like you got exactly 21 points.. Automatic win!")
     else:
+        # if the user didn't reach 21, this has the dealer draw 2 cards, informs the user on what the dealer has
         dealer_card_1 = get_card()
         dealer_card_2 = get_card()
         dealer_total = dealer_card_1+dealer_card_2
         print("The dealer drew a "+str(dealer_card_1)+" and a "+str(dealer_card_2)+
               ", which makes a total of "+str(dealer_total))
+        # delivers the verdict on who won based on the dealer and user total by calling the get_winner function
         print(get_winner(user_total, dealer_total))
 
 
